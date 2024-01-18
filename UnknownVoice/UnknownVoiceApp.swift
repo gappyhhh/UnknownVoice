2fcf4d6fa699fae972ce62f4b89b4863048df72c
gappyhhh <gappyhhh9999@gmail.com>
2024-01-18 21:49:49 +0900

    Initial Commit

---
+++ UnknownVoice/UnknownVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnknownVoiceApp.swift
+//  UnknownVoice
+//
+//  Created by kazuhiro hirata on 2024/01/18.
+//
+
+import SwiftUI
+
+@main
+struct UnknownVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
